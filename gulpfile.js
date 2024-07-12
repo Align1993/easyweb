5ad20c24e745af8603ceb6465e9464f698e80ca0
bigant <260083304@qq.com>
2024-07-12 10:26:16 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,57 @@
-"use strict";
+'use strict';
 //是否需要压缩
 const compress = false;
-const webpack = require("webpack");
-const webpackconfig = require("./webpack.config");
-const { parallel, series, src, dest, watch } = require("gulp");
-const fileinclude = require("gulp-file-include");
-const revAppend = require("./plugins/rev-append");
-const uglify = require("gulp-uglify"); //js压缩
-const minifycss = require("gulp-minify-css"); //css压缩
-const spritesmith = require("gulp.spritesmith"); //合并雪碧图
-const javascriptObfuscator = require("gulp-javascript-obfuscator"); //混淆js
+// 引入 path 模块
+const path = require('path');
+const webpack = require('webpack');
+const webpackconfig = require('./webpack.config');
+const { parallel, series, src, dest, watch } = require('gulp');
+const fileinclude = require('gulp-file-include');
+const revAppend = require('./plugins/rev-append');
+const uglify = require('gulp-uglify'); //js压缩
+const minifycss = require('gulp-minify-css'); //css压缩
+const spritesmith = require('gulp.spritesmith'); //合并雪碧图
+const javascriptObfuscator = require('gulp-javascript-obfuscator'); //混淆js
 
-const postCss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const pxtorem = require("postcss-pxtorem");
-const sass = require("gulp-sass");
-const parseArgs = require("minimist");
-const gutil = require("gulp-util");
-const imagemin = require("gulp-imagemin");
-sass.compiler = require("node-sass");
+const postCss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const pxtorem = require('postcss-pxtorem');
+const sass = require('gulp-sass');
+const parseArgs = require('minimist');
+const gutil = require('gulp-util');
+const imagemin = require('gulp-imagemin');
+sass.compiler = require('node-sass');
 var argv = parseArgs(process.argv);
-var webserver = require("gulp-webserver");
-const preprocess = require("gulp-preprocess");
-const babel = require("gulp-babel");
+var webserver = require('gulp-webserver');
+const preprocess = require('gulp-preprocess');
+const babel = require('gulp-babel');
 
 function webapp(cb) {
-  return src("dist").pipe(
+  return src('dist').pipe(
     webserver({
       port: 8084,
-      host: "localhost",
+      host: 'localhost',
       livereload: true,
       open: true,
       directoryListing: {
-        path: "index.html", //你web资源的起始页，在dist目录下
+        path: 'index.html', //你web资源的起始页，在dist目录下
         enable: true,
       },
       proxies: [
         {
-          source: "/api",
-          target: "http://jiuqitech.cn/api",
+          source: '/api',
+          target: 'http://jiuqitech.cn/api',
         },
         {
-          source: "/portal",
-          target: "http://jiuqitech.cn/portal",
+          source: '/portal',
+          target: 'http://jiuqitech.cn/portal',
         },
       ],
       // {
       //   source:'/usual-api',target:'http://175.24.154.162/'
       // }
       // ]
-    }),
+    })
   );
   cb();
 }
@@ -58,15 +60,16 @@ function webapp(cb) {
  * @function html处理
  */
 function moveHtml(cb) {
-  return src("src/**/*.html")
+  return src('src/**/*.html')
     .pipe(revAppend())
     .pipe(
       fileinclude({
-        prefix: "@@",
-        basepath: "@file",
-      }),
+        prefix: '@@',
+        basepath: '@file',
+        // basepath: path.join(__dirname, 'src'), // 绝对路径到src目录
+      })
     )
-    .pipe(dest("dist/"));
+    .pipe(dest('dist/'));
   cb();
 }
 
@@ -75,20 +78,20 @@ function moveHtml(cb) {
  */
 function moveCss(cb) {
   //   return src(["src/**/*.css","!src/**/*.min.css","!src/**/videocss.css"]).pipe(minifycss()).pipe(dest("dist/"));
-  return src("src/**/*.css").pipe(minifycss()).pipe(dest("dist/"));
+  return src('src/**/*.css').pipe(minifycss()).pipe(dest('dist/'));
   cb();
 }
 //生成filename文件，存入string内容
 function string_src(filename, string) {
-  var src = require("stream").Readable({ objectMode: true });
+  var src = require('stream').Readable({ objectMode: true });
   src._read = function () {
     this.push(
       new gutil.File({
-        cwd: "",
-        base: "",
+        cwd: '',
+        base: '',
         path: filename,
         contents: new Buffer(string),
-      }),
+      })
     );
     this.push(null);
   };
@@ -99,10 +102,10 @@ function string_src(filename, string) {
  * @function 配置文件
  */
 function createConfig(cb) {
-  var myConfig = require("./config");
+  var myConfig = require('./config');
   var envConfig = myConfig[argv.env];
-  var conConfig = "var APPCONFIG = " + JSON.stringify(envConfig);
-  return string_src("config.js", conConfig).pipe(dest("dist/js/"));
+  var conConfig = 'var APPCONFIG = ' + JSON.stringify(envConfig);
+  return string_src('config.js', conConfig).pipe(dest('dist/js/'));
   cb();
 }
 
@@ -110,17 +113,16 @@ function createConfig(cb) {
  * @function js处理
  */
 function moveJs(cb) {
-   // 处理 src/libs 目录下的 js 文件，仅进行输出
-   src("src/libs/**/*.js")
-   .pipe(dest("dist/libs/"));
-  return src("src/js/*.js")
+  // 处理 src/libs 目录下的 js 文件，仅进行输出
+  src('src/libs/**/*.js').pipe(dest('dist/libs/'));
+  return src('src//*.js')
     .pipe(
       preprocess({
         context: {
           // 此处可接受来自调用命令的 NODE_ENV 参数，默认为 development 开发测试环境
-          NODE_ENV: argv.env || "development",
+          NODE_ENV: argv.env || 'development',
         },
-      }),
+      })
     )
     .pipe(javascriptObfuscator())
     .pipe(babel())
@@ -132,9 +134,9 @@ function moveJs(cb) {
           drop_console: false, // 过滤 console
           drop_debugger: true, // 过滤 debugger
         },
-      }),
+      })
     )
-    .pipe(dest("dist/"));
+    .pipe(dest('dist/'));
   cb();
 }
 
@@ -148,51 +150,51 @@ function translateSass(cb) {
       rootValue: 192,
       unitPrecision: 2,
       propList: [
-        "width",
-        "min-width",
-        "height",
-        "min-height",
-        "font",
-        "top",
-        "bottom",
-        "left",
-        "right",
-        "font-size",
-        "line-height",
-        "letter-spacing",
-        "margin",
-        "margin-top",
-        "margin-left",
-        "margin-bottom",
-        "margin-right",
-        "padding",
-        "padding-top",
-        "padding-left",
-        "padding-bottom",
-        "padding-right",
-        "background-size",
-        "background",
-        "text-indent",
-        "border",
-        "flex-basis",
-        "border-top",
-        "border-bottom",
-        "border-right",
-        "border-left",
+        'width',
+        'min-width',
+        'height',
+        'min-height',
+        'font',
+        'top',
+        'bottom',
+        'left',
+        'right',
+        'font-size',
+        'line-height',
+        'letter-spacing',
+        'margin',
+        'margin-top',
+        'margin-left',
+        'margin-bottom',
+        'margin-right',
+        'padding',
+        'padding-top',
+        'padding-left',
+        'padding-bottom',
+        'padding-right',
+        'background-size',
+        'background',
+        'text-indent',
+        'border',
+        'flex-basis',
+        'border-top',
+        'border-bottom',
+        'border-right',
+        'border-left',
       ],
     }),
   ];
-  return src("src/scss/*.scss")
+  return src('src/scss/*.scss')
     .pipe(sass())
     .pipe(postCss(plugins))
-    .pipe(dest("src/css/"));
+    .pipe(dest('src/css/'));
   cb();
 }
 
 function webpackcall(cb) {
   return new Promise((resolve, reject) => {
     webpack(webpackconfig, function (err, stats) {
-      console.log("webpack called");
+      console.log('webpack called');
     });
   });
   cb();
@@ -202,16 +204,16 @@ function webpackcall(cb) {
  * @function 压缩图片
  */
 function imageCache(cb) {
-  return src("src/resources/**/*.{png,jpg,jpeg,gif,ico}")
+  return src('src/resources/**/*.{png,jpg,jpeg,gif,ico}')
     .pipe(
       imagemin({
         optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
         progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
         interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
         multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
-      }),
+      })
     )
-    .pipe(dest("dist/resources/"));
+    .pipe(dest('dist/resources/'));
   cb();
 }
 
@@ -219,7 +221,7 @@ function imageCache(cb) {
  * @function 生成资源文件
  */
 function moveResources(cb) {
-  return src("src/resources/**/*.*").pipe(dest("dist/resources/"));
+  return src('src/resources/**/*.*').pipe(dest('dist/resources/'));
   cb();
 }
 
@@ -229,14 +231,14 @@ function moveResources(cb) {
 function spriteImg(cb) {
   let spriteArr = [
     {
-      src: "src/resources/images/navIcon/*.*",
-      imgName: "resources/images/icons/navIconSprite.png",
-      cssName: "css/navIconSprite.css",
+      src: 'src/resources/images/navIcon/*.*',
+      imgName: 'resources/images/icons/navIconSprite.png',
+      cssName: 'css/navIconSprite.css',
     },
     {
-      src: "src/resources/images/culture/partner_logo/*.*",
-      imgName: "resources/images/icons/partner_logo.png",
-      cssName: "css/partner_logo.css",
+      src: 'src/resources/images/culture/partner_logo/*.*',
+      imgName: 'resources/images/icons/partner_logo.png',
+      cssName: 'css/partner_logo.css',
     },
   ];
 
@@ -247,76 +249,76 @@ function spriteImg(cb) {
           imgName: spriteArr[i].imgName, //保存合并后的名称
           cssName: spriteArr[i].cssName, //保存合并后css样式的地址
           padding: 20, //合并时两个图片的间距
-          algorithm: "binary-tree", //注释1
+          algorithm: 'binary-tree', //注释1
           cssTemplate: function (data) {
             //如果是函数的话，这可以这样写
             var arr = [];
             data.sprites.forEach(function (sprite) {
-              var x = (sprite.px.offset_x.replace("px", "") * 100) / 1920;
-              var y = (sprite.px.offset_y.replace("px", "") * 100) / 1920;
+              var x = (sprite.px.offset_x.replace('px', '') * 100) / 1920;
+              var y = (sprite.px.offset_y.replace('px', '') * 100) / 1920;
 
               arr.push(
-                "." +
+                '.' +
                   sprite.name +
-                  "{" +
+                  '{' +
                   "background-image: url('" +
                   sprite.escaped_image +
                   "');" +
-                  "background-repeat: no-repeat;" +
-                  "background-position: " +
+                  'background-repeat: no-repeat;' +
+                  'background-position: ' +
                   x +
-                  "vw" +
-                  " " +
+                  'vw' +
+                  ' ' +
                   y +
-                  "vw" +
-                  ";" +
-                  "background-size: " +
+                  'vw' +
+                  ';' +
+                  'background-size: ' +
                   ((sprite.total_width / 1920) * 100).toFixed(2) +
-                  "vw auto;" +
+                  'vw auto;' +
                   // "width:"+sprite.px.width+";"+
                   // "height:"+sprite.px.height+";"+
-                  "}\n",
+                  '}\n'
               );
             });
-            return arr.join("");
+            return arr.join('');
           },
-        }),
+        })
       )
-      .pipe(dest("dist/"));
+      .pipe(dest('dist/'));
   }
   cb();
 }
 
 function node(cb) {
   // cry
-  src("node_modules/crypto-js/crypto-js.js").pipe(dest("dist/js"));
+  src('node_modules/crypto-js/crypto-js.js').pipe(dest('dist/js'));
   cb();
   // cry
-  src("node_modules/jsencrypt/bin/jsencrypt.min.js").pipe(dest("dist/js"));
+  src('node_modules/jsencrypt/bin/jsencrypt.min.js').pipe(dest('dist/js'));
   cb();
 }
 
 const watcher = watch([
-  "src/scss/*.scss",
-  "src/js/*.js",
-  "src/resources/**/*.*",
-  "src/**/*.html",
+  'src/scss/*.scss',
+  'src/js/*.js',
+  'src/resources/**/*.*',
+  'src/**/*.html',
 ]);
 
 /**
  * @function ico处理
  */
 function translateIco() {
-  return src("src/resources/favicon.ico").pipe(dest("dist/resources/"));
+  return src('src/resources/favicon.ico').pipe(dest('dist/resources/'));
 }
 
 watcher.on(
-  "change",
+  'change',
   series(
     // translateSass,
     // parallel(moveCss, moveJs, spriteImg, moveResources, moveHtml),
-    parallel(moveCss, moveJs, moveResources, moveHtml),
-  ),
+    parallel(moveCss, moveJs, moveResources, moveHtml)
+  )
 );
 
 exports.build = series(
@@ -324,5 +326,5 @@ exports.build = series(
   node,
   parallel(moveCss, moveJs, moveResources, moveHtml),
   // parallel(moveCss, moveJs, spriteImg, moveResources, moveHtml),
-  webapp, //webpackcall
+  webapp //webpackcall
 );
